2439fb4ce167cc2927fe7d19225e7f61b6b12154
surafelEmu <surafel097@outlook.com>
2022-04-09 13:11:29 +0300

    created a vite + vutify project with tailwindCss

---
+++ fe/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <SignUp />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    SignUp
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ fe/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html" ,
+    "./src/components/SignUp.vue" 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
